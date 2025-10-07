ae02ee549e59a1c7a5c48acf5665f4fa9ada7665
dheerajs07 <singhdheeru901@gmail.com>
2025-10-07 22:04:55 +0530

    API and Schema Validations

--- src/app.js
+++ src/app.js
@@ -2,16 +2,16 @@ const express= require('express');
 const {auth}= require('./middleware/Auth')
 const {connectDB}= require('./config/database');
 const app= express();
-const User= require('./model/user')
+const User= require('./model/user');
+const { after } = require('node:test');
 
 app.use(express.json());
 
 app.get("/user", async (req,res)=>{
     const body= req.body.emailId;
-    console.log(body);
     try{
 
-        const user=await  User.find({emailId:body});
+        const user=await  User.find({emailId:body} );
         if(user.length ===0){
             res.send("User not found");
 
@@ -60,16 +60,32 @@ app.delete("/user", async (req,res)=>{
     }
 })
 
-app.patch("/user", async(req,res)=>{
-    const userId= req.body._id;
-    const name= req.body.name;
-
+app.patch("/user/:userId", async(req,res)=>{
+    const userId= req.params?.userId;
+    const data= req.body;
+    
     try{
-        const user = await User.findByIdAndUpdate({_id:userId}, {firstName:name})
+        const ALLOWED_UPDATES=["gender","age","about","photoUrl","Skills"];
+        const isAllowed= Object.keys(data).every((k)=>
+            ALLOWED_UPDATES.includes(k)
+        )
+        if(!isAllowed){
+            throw new Error("Update not allowed .")
+        }
+        
+        if(data?.Skills?.length>10){
+            throw new Error("Skills cannot be greater than 10")
+        }
+
+        const user = await User.findByIdAndUpdate({_id:userId}, data, {
+            runValidators:true,
+            returnDocument:after,
+        })
         res.send("User updated")
         console.log(user);
-    }catch(err){
-        res.status("Something went wrong ");
+    }
+    catch(err){
+        res.status(400).send("Something went wrong " + err.message);
     }
 } )
 
--- src/model/user.js
+++ src/model/user.js
@@ -1,20 +1,76 @@
 const mongoose= require('mongoose');
+const validator= require('validator');
 
 const userSchema= new mongoose.Schema({
     firstName:{
         type:String,
+        required:true,
+        minLength:3,
+        maxLength:25,
+        trim:true,
     },
     lastName:{
         type:String,
+        minLength:3,
+        maxLength:25,
+        trim:true,
     },
     emailId:{
         type:String,
+        lowercase:true,
+        trim:true,
+        required:true,
+        unique:true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error("Invalid Email address" + value)
+            }
+        } ,
+    },
+    gender:{
+        type:String,
+        required:true,
+        validate(value){
+            if(!["male","female","others"].includes(value)){
+                throw new Error("Invalid gender input");
+            }
+        },
     },
     password:{
         type:String,
+        required:true,
+        minLength:8,
+        maxLength:12,
+         validate(value){
+            if(!validator.isStrongPassword(value)){
+                throw new Error("Weak Password!! Re-enter a strong password" + value)
+            }
+        } 
     },
-    
+    age:{
+        type:Number,
+        required:true,
+        min:18,
+    },
+    photoUrl:{
+        type:String,
+        default:"https://cyber.comolho.com/static/img/avatar.png",
+        validate(value){
+            if(!validator.isURL(value)){
+                throw new Error("Invalid URL" + value)
+            }
+        } 
+    },
+    about:{
+        type:String,
+        default:"This is default about the user!!!"
+    },
+    Skills:{
+        type:[String],
+    },
 
+},{
+    timestamps:true,
 })
 
 module.exports= mongoose.model("User",userSchema);
