412be75fe3ac6714ae69a2578a22464064e2c513
dheerajs07 <singhdheeru901@gmail.com>
2025-12-06 21:08:56 +0530

    Implemented feed api with pagination .

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,6 +2,7 @@ const express= require("express");
 const userRouter= express.Router();
 const sendConnection= require('../model/sendConnection');
 const {userAuth}= require('../middleware/Auth')
+const User= require('../model/user');
 userRouter.get("/user/requests/received", userAuth, async(req,res)=>{
     try{
         const loggedInUser= req.user;
@@ -38,6 +39,38 @@ userRouter.get("/user/connections", userAuth , async(req,res)=>{
     }
 })
 
+userRouter.get("/feed", userAuth , async(req,res)=>{
+    try{
+        const loggedInUser= req.user;
+        const limit= parseInt(req.query.limit) || 10;
+        const page= parseInt(req.query.page) || 1;
+        const skip = (page-1) * limit;
 
+        const connectionRequests= await sendConnection.find({
+            $or:[
+                {toUserId:loggedInUser._id},
+                {fromUserId:loggedInUser._id}
+            ],
+        }).select("toUserId fromUserId");
+
+        const userIdtobeHidden= new Set();
+
+        connectionRequests.forEach((item)=>{
+            userIdtobeHidden.add(item.toUserId.toString());
+            userIdtobeHidden.add(item.fromUserId.toString());
+        })
+
+        userIdtobeHidden.add(loggedInUser._id.toString());
+        const data= await User.find({
+            _id:{$nin: Array.from(userIdtobeHidden)},
+        }).select("firstName lastName age gender photoUrl about skills")
+        .skip(skip)
+        .limit(limit);
+
+        res.status(200).send({data});
+    }catch(err){
+        res.status(400).send("error : " + err.message);
+    }
+})
 
 module.exports= userRouter;
